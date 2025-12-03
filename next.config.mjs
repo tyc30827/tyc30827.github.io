86af0ca38fdd7fa1dc5c2f1705ec5e8b4286f30b
willie <willie.huang@xrspace.io>
2025-12-03 23:31:42 +0800

    fix: fix deloyment issue

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,11 @@
 const nextConfig = {
   /* config options here */
   output: 'export',
-       images: {
-        unoptimized: true,
-       },
+  images: {
+    unoptimized: true,
+  },
+  // Ensure trailing slashes for GitHub Pages
+  trailingSlash: true,
 };
 
 export default nextConfig;
